11a76c503f95b8b99f72cf753e21fe1c053db281
Ginish P George <ginishumn@gmail.com>
2017-06-13 10:03:26 +0530

    Implemented successful executor script for click()

--- src/Auto/Calculator.java
+++ src/Auto/Calculator.java
@@ -1,6 +1,8 @@
 package Auto;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Calculator {
@@ -9,7 +11,7 @@ public class Calculator {
 		// TODO Auto-generated method stub
 		FirefoxDriver driver = new FirefoxDriver();
 		driver.get("http://www.calculator.net");
-driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[1]/table/tbody/tr[2]/td[2]/div/div[1]/span[2]"));
+WebElement element = driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[1]/table/tbody/tr[2]/td[2]/div/div[1]/span[2]"));
 JavascriptExecutor executor = (JavascriptExecutor)driver;
 executor.executeScript("arguments[0].click();", element);
 	}
