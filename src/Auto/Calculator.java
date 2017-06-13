529279f95211ac0e2288588246c55eaf34261620
Ginish George <Ginish@peoplytics-machine13.local.hirrr.com>
2017-06-13 09:55:08 +0530

    Initial project

---
+++ src/Auto/Calculator.java
@@ -0,0 +1,14 @@
+package Auto;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://www.calculator.net");
+
+	}
+
+}
