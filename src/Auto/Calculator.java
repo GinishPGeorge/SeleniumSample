9ffbe6b61375330347c80034c9db31f620386c76
Ginish P George <ginishumn@gmail.com>
2017-06-13 10:23:41 +0530

    implemented initial test

--- src/Auto/Calculator.java
+++ src/Auto/Calculator.java
@@ -9,16 +9,20 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Calculator {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		FirefoxDriver driver = new FirefoxDriver();
 		driver.get("http://www.calculator.net");
 WebElement element = driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[1]/table/tbody/tr[2]/td[2]/div/div[1]/span[2]"));
 JavascriptExecutor executor = (JavascriptExecutor)driver;
 executor.executeScript("arguments[0].click();", element);
+
+driver.wait(3000);
 WebElement a=driver.findElement(By.id("sciOutPut"));
-System.out.println("The output is " +a.getText());
-//assert.assertEquals(actual, expected);
+String b = a.getText();
+System.out.println("The output is " +b);
+
+assertEquals(b, "8");
 	}
 
 }
