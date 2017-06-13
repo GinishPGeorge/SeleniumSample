1859d1dccec470f136ee69888042458307abfc0b
Ginish P George <ginishumn@gmail.com>
2017-06-13 10:28:28 +0530

    Removed assert

--- src/Auto/Calculator.java
+++ src/Auto/Calculator.java
@@ -2,10 +2,13 @@ package Auto;
 
 import static org.testng.Assert.assertEquals;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.server.handler.ImplicitlyWait;
 
 public class Calculator {
 
@@ -17,12 +20,11 @@ WebElement element = driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[1
 JavascriptExecutor executor = (JavascriptExecutor)driver;
 executor.executeScript("arguments[0].click();", element);
 
-driver.wait(3000);
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 WebElement a=driver.findElement(By.id("sciOutPut"));
 String b = a.getText();
 System.out.println("The output is " +b);
 
-assertEquals(b, "8");
 	}
 
 }
