54e8fd8cd845facbc672a5131b03c135f432fc44
Ginish P George <ginishumn@gmail.com>
2017-06-13 10:00:28 +0530

    Revision to exector mode

--- src/Auto/Calculator.java
+++ src/Auto/Calculator.java
@@ -1,5 +1,6 @@
 package Auto;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Calculator {
@@ -8,7 +9,9 @@ public class Calculator {
 		// TODO Auto-generated method stub
 		FirefoxDriver driver = new FirefoxDriver();
 		driver.get("http://www.calculator.net");
-driver.findElementsByClassName("scinm");
+driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[1]/table/tbody/tr[2]/td[2]/div/div[1]/span[2]"));
+JavascriptExecutor executor = (JavascriptExecutor)driver;
+executor.executeScript("arguments[0].click();", element);
 	}
 
 }
